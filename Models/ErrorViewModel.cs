c2269cb3a71754ed781dacd28535d7f8c7af823c
Suraysen Thiyagarasu <suraysen@yahoo.com>
2023-07-13 17:14:05 +0800

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Net6_Work1.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}

09c1e9c0c44cdf21d81132a3ea32e36643b494af
Suraysen Thiyagarasu <suraysen@yahoo.com>
2023-07-15 12:43:47 +0800

    first draft

---
+++ Controllers/CategoryController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Net6_Work1.Data;
+using Net6_Work1.Models;
+
+namespace Net6_Work1.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDBContext _db;
+        public CategoryController(ApplicationDBContext db)
+        {
+            _db= db;
+        }
+
+        public IActionResult Index()
+        {
+            IEnumerable<Category> listModel = _db.Categories;
+            return View(listModel);
+        }
+    }
+}
---
+++ Data/ApplicationDBContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Net6_Work1.Models;
+using System.Data;
+
+namespace Net6_Work1.Data
+{
+    public class ApplicationDBContext:DbContext
+    {
+        public ApplicationDBContext(DbContextOptions<ApplicationDBContext> options):base (options)
+        {
+                
+        }
+
+        public DbSet<Category> Categories { get; set; }      
+    }
+}
